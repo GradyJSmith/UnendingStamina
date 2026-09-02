990f42fbb68e7ef93a0c516718c7ed9a7d840e10
BloYoMind <grady.jeremiah.smith@gmail.com>
2026-09-01 20:38:39 -0500

    v1.0.0 code

---
+++ LC_FirstMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace LC_FirstMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void infiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+
+    }
+}
---
+++ LC_FirstMod/Pluggin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LC_FirstMod.Patches;
+
+namespace UnendingStamina
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+
+    public class UnendingStaminaBase : BaseUnityPlugin
+    {
+        //setup
+        private const string modGUID = "GStar.UnendingStamina";
+        private const string modName = "UnendingStamina";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static UnendingStaminaBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Unending stamina has been activated");
+
+            harmony.PatchAll(typeof(UnendingStaminaBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
